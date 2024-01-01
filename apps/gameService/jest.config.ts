1f7f63d14d4b24517ab3077981249d778aaabf46
admin <admin@admins-MBP.lan>
2024-01-01 20:36:27 +0200

    init repo

--- apps/gameService/jest.config.ts
+++ apps/gameService/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'game2048',
+  displayName: 'gameService',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/game2048'
+  coverageDirectory: '../../coverage/apps/gameService'
 };
